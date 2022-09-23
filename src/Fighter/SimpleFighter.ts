df24849c8d9f844b9d27b2ac266f84262714302d
Caio Galvão <caio_bgalvao@hotmail.com>
2022-09-23 00:09:35 +0000

    feat: requisito 8

---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,9 @@
+interface SimpleFighter {
+  lifePoints: number;
+  strength: number;
+
+  attack(enemy: SimpleFighter): void;
+  receiveDamage(attackPoint:number):number
+}
+
+export default SimpleFighter;
